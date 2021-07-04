bd943e69e2224fc2edb38f83830ee97d1782b0b5
Md Azizur Rahman (Hakim) <57495952+Azizur-Rahman-CSE@users.noreply.github.com>
2021-07-04 23:56:29 +0600

    Update All pair sortest path finding code.cpp

--- All pair sortest path finding code.cpp
+++ All pair sortest path finding code.cpp
@@ -9,14 +9,13 @@ int ar[505][505] ;
 
 void Min(int &a, int b) {
     if (b<a) a = b ; 
-    return  ; 
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); 
 
-    int n, m, ans = 0 ; 
+    int n, m ; 
     cin >> n >> m ; 
     for (int i=1;i<=n;++i) {
         for (int j=1;j<=n;++j) {
